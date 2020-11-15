4d232c61405c49c962adb5e2186af5cfcbf47c4e
Valentin Cernuta <vcernuta3@gmail.com>
2020-11-15 23:15:22 +0100

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Valentin Cernuta on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
